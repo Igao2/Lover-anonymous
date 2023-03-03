6d5729f40cdf129beda67211bac542c507a0d458
Igao2 <igor.dasilvacosta8@gmail.com>
2023-03-03 13:25:07 -0300

    Direct message working

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require ("express");
+//import { IgApiClient } from 'instagram-private-api';
+//import { sample } from 'lodash';
 const diretorio = require("path");
+const { IgApiClient } = require('instagram-private-api');
+const { AccountRepository } = require("instagram-private-api/dist/repositories/account.repository");
+const { log } = require("console");
 const app = express();
-app.use(express.json()); 
+var sexo =""
+var teze =""
+app.use(express.json());
+const ig =  new IgApiClient();
+
+const username = "loversecretanonymous";
+const pass = "Cientista232@"
+ig.state.generateDevice(username);
+const repository = new AccountRepository(ig)
+ig.state.proxyUrl = teze;
+ig.account.login(username,pass);
+
 app.use(express.urlencoded({extended:true}));
 app.get("/",function(req,res)
 {
     res.sendFile(__dirname+"/main.html");
 });
-app.post('/',(req,res) =>
+
+
+app.post('/', async (req,res) =>
 {
-    request = req.body.text;
-    request1 = req.body.insta;
+    request = req.body.text
+    request1 = req.body.insta
     var insta = request1;
     var text = request;
+    const userId = await ig.user.getIdByUsername(insta);
+    console.log(userId);
+    const thread = ig.entity.directThread([userId.toString()]);
+    await thread.broadcastText(text);
     console.log(text);
     console.log(insta);
 
 });
+
 
 app.listen(8080,function(){console.log("Server Rodando")});
